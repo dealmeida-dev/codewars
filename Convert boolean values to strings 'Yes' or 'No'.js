47c6c6201ce4ce5b27edc7b399a6f3e47ec5d04b
Delfim Almeida <dealmeida.dev@gmail.com>
2022-12-08 16:40:57 +0100

    added new challenge

---
+++ Convert boolean values to strings 'Yes' or 'No'.js
@@ -0,0 +1,11 @@
+// The challenge
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// My Solution
+
+function boolToWord( bool ){
+  return (bool == true ? "Yes" : "No")
+}
+
+// Used a Conditional (ternary) operator to understand if the boolean value was true or false
