a0808503f39b3a51df2fbd3c6625df4df916d5fa
dealmeida-dev <102060897+dealmeida-dev@users.noreply.github.com>
2023-01-09 15:23:46 +0100

    added new JS challenge

---
+++ Convert a Boolean to a String.js
@@ -0,0 +1,9 @@
+// The challenge:
+
+// Implement a function which convert the given boolean value into its string representation.
+
+//Solution using .toString method
+
+function booleanToString(b){
+  return b.toString();
+}
