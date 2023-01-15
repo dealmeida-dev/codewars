4ac4908b09853c172ba752758882f4a552dcfb66
dealmeida-dev <102060897+dealmeida-dev@users.noreply.github.com>
2023-01-15 21:11:30 +0100

    added new 8kyu challenge

---
+++ 8kyu/Simple multiplication.js
@@ -0,0 +1,7 @@
+// The challenge:
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+//Solution
+
+const simpleMultiplication = number => number % 2 == 0 ? number * 8 : number * 9;
