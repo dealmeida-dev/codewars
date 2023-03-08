4318a6c9027ebf77d4966bc78c2a4b9b1b6b74f0
dealmeida-dev <102060897+dealmeida-dev@users.noreply.github.com>
2023-03-08 19:06:22 +0100

    new challenge

---
+++ 8kyu/Classy Extentions.js
@@ -0,0 +1,14 @@
+// The challenge 
+
+/*Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+*/
+
+
+// My solution 
+
+
+class Cat extends Animal {
+  speak() {
+  return `${this.name} meows.`;
+}
+}
